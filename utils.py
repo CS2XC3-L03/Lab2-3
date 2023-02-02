7db65e04f06c3e025faa9aa6ad80d54335087275
Saad T <tariqs26@mcmaster.ca>
2023-02-02 00:52:39 -0500

    IMPORTANT FIX, LIST COPY IS USED

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ def time_sort(sort, L, num_of_runs):
     sum = 0
     for _ in range(num_of_runs):
         start = t.default_timer()
-        sort(L)
+        sort(L[:])
         end = t.default_timer()
         sum += end - start
     return sum / num_of_runs
